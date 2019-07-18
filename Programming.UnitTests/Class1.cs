9c327e237bb08fc55ea2e77d8f72d5585c784e1e
Trio556 <alemat04@gmail.com>
2019-07-17 21:04:56 -0400

    Removed the other projects for now since I'm just focusing on Datastructures currently

--- Programming.UnitTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Programming.UnitTests
-{
-    public class Class1
-    {
-    }
-}
--- Programming/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Programming
-{
-    public class Class1
-    {
-    }
-}
